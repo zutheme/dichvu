2c4868418feb0b8fb4b73586e3d126c215d22b86
hatazu <hatazu@gmail.com>
2020-02-20 17:39:43 +0700

    s

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -18,7 +18,7 @@ class ComposerServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    private $main_menu;
+    private $main_menu = '';
     public function boot()
     {
         //View::composer(['admin.dashboard','dashboard'], function ($view) {
@@ -68,21 +68,11 @@ class ComposerServiceProvider extends ServiceProvider
         $result = DB::select('call ListAllCatByTypeProcedure(?)',array($_cattype));
         $categories = json_decode(json_encode($result), true);
         $str_ul="";$str_li="";
-        if($_idcat > 0){
-           $this->showCategories($categories, $_idcat,0,$_cate_selected);
-           $s_catename = DB::select('call SelRowCategoryByIdProcedure(?)',array($_idcat));
-           $r_catename = json_decode(json_encode($s_catename), true);
-           foreach ($r_catename as $item) {
-               //$selected = ($this->compare_in_list($_cate_selected,$item['idcategory']) >0) ? 'checked' : '';
-               $str_li = '<li>'.$item['namecat'];
-            }
-       }else{
-           $this->showCategories($categories, 0,0,$_cate_selected);
-       }      
+        $this->showCategories($categories, 0, 0);   
         $str_html = $str_li.$this->main_menu;
         return $str_html; 
     }
-    public function showCategories($categories, $idparent = 0, $char = 0, $_cate_selected)
+    public function showCategories($categories, $idparent = 0, $level = 0)
     {
         $cate_child = array();
         foreach ($categories as $key => $item){
@@ -94,14 +84,22 @@ class ComposerServiceProvider extends ServiceProvider
         }
         $list_cat="";       
         if ($cate_child){
-            $char++;
-            $this->main_menu .= '<div class="menu_section '.$char.'"><ul class="nav side-menu">';
+            if($level == 0){
+                $this->main_menu .= '<div class="menu_section depth-'.$level.'"><ul class="nav side-menu">';
+            }else{
+                $this->main_menu .= '<ul class="nav child_menu depth-'.$level.'">';
+            }
             foreach ($cate_child as $key => $item){
-                $this->main_menu .= '<li>'.$item['namecat'];
-                $this->showCategories($categories, $item['idcategory'], $char, $_cate_selected);
+                $this->main_menu .= '<li><a href="#">'.$item['namecat'].'</a>';
+                $this->showCategories($categories, $item['idcategory'], $level+1);
                 $this->main_menu .= '</li>';
             }
-            $this->main_menu .= '</ul></div>';
+            if($level == 0){
+                $this->main_menu .= '</ul></div>';
+            }else{
+                $this->main_menu .= '</ul>';
+            }
+            
         }
     }
 }
